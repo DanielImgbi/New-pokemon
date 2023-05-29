39184fb612252c2763cd9b5577f1bf5f6b17c1ea
igdanie13 <danielgodspowerimgbi@gmail.com>
2023-05-29 06:53:47 +0500

    initiallizing

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./**/**/*{.tsx,ts}', 'index.html'],
+  theme: {
+    extend: {
+      height:{
+        // px
+        '150px': '150px',
+        '250px': '250px',
+  
+        // percentage
+        '20%':'20%',
+        '50%':'50%',
+        '60%':'60%',
+        '80%': '80%',
+        '90%': '90%',
+        '100%': '100%',
+        //view height
+          
+        '100vh': '100vh',
+      },
+      width:{
+        //%
+        '50%':'50%',
+        '60%':'60%',
+        // vw
+        '100vw':'100vw'
+      },
+    },
+  },
+  plugins: [],
+}
+
